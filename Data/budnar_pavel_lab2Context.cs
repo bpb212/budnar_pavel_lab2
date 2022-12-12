4e09931e95eeeb9cf942c0ea0a7d1f542bb1d89d
Budnar Pavel-Bogdan <bpavel_94@hotmail.com>
2022-12-12 17:27:15 +0200

    Add project files.

---
+++ Data/budnar_pavel_lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using budnar_pavel_lab2.Models;
+
+namespace budnar_pavel_lab2.Data
+{
+    public class budnar_pavel_lab2Context : DbContext
+    {
+        public budnar_pavel_lab2Context (DbContextOptions<budnar_pavel_lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<budnar_pavel_lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<budnar_pavel_lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
